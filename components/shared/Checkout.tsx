4740af45ac45b55771f5facf786f85b669f560c9
jOsterrielt <jorgeosterrielt@gmail.com>
2024-04-02 19:23:25 -0300

    import error fixed

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -4,7 +4,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { useEffect } from "react";
 
 import { useToast } from "@/components/ui/use-toast";
-import { checkoutCredits } from "@/lib/actions/transaction.action";
+import { checkoutCredits } from "@/lib/actions/transaction.actions";
 
 import { Button } from "../ui/button";
 
